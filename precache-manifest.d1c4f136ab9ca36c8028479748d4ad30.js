58e7b37676818bf972e6d549efe00c1e254ecff0
Nan Ma <nanma@microsoft.com>
2019-07-26 16:41:03 -0700

    Updates

---
+++ precache-manifest.d1c4f136ab9ca36c8028479748d4ad30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f72f707ef4d8839dc6fcb46fb99ce2",
+    "url": "/star/index.html"
+  },
+  {
+    "revision": "af4e8ae179cf90e71eb1",
+    "url": "/star/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/star/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "af4e8ae179cf90e71eb1",
+    "url": "/star/static/js/main.9bbd2b1c.chunk.js"
+  },
+  {
+    "revision": "2898011e16a63222a549",
+    "url": "/star/static/js/runtime~main.81a9ca3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/star/static/media/logo.5d5d9eef.svg"
+  }
+]);

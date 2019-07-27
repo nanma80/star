fa5b4c8d90303d972067b126ca4c63e61b3001cb
Nan Ma <nanma@microsoft.com>
2019-07-26 17:03:06 -0700

    start side bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React from "react";
+import Sidebar from "react-sidebar";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const mql = window.matchMedia(`(min-width: 800px)`);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarDocked: mql.matches,
+      sidebarOpen: false
+    };
+
+    this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  }
+
+  componentWillMount() {
+    mql.addListener(this.mediaQueryChanged);
+  }
+
+  componentWillUnmount() {
+    mql.removeListener(this.mediaQueryChanged);
+  }
+
+  onSetSidebarOpen(open) {
+    this.setState({ sidebarOpen: open });
+  }
+
+  mediaQueryChanged() {
+    this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
+  }
+
+  render() {
+    return (
+      <Sidebar
+        sidebar={<b>Sidebar content</b>}
+        open={this.state.sidebarOpen}
+        docked={this.state.sidebarDocked}
+        onSetOpen={this.onSetSidebarOpen}
+      >
+
+        <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,7 +58,9 @@ function App() {
         </a>
       </header>
     </div>
-  );
+      </Sidebar>
+    );
+  }
 }
 
 export default App;

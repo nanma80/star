072e0a6fe0dbcb92ac46ce90ce898c2e9721f7e0
Nan Ma <nanma@microsoft.com>
2021-02-01 15:23:57 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/star/precache-manifest.956ba8c110155217c9be817ad461b336.js"
+  "/star/precache-manifest.87db3658c33f9128669d88941f182962.js"
 );
 
 self.addEventListener('message', (event) => {

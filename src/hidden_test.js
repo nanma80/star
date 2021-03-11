9e51730fdfffe181ab2e4c94a76025a607f1308f
Nan Ma <nanma@microsoft.com>
2021-03-11 15:34:29 -0800

    update vzome package

--- src/hidden_test.js
+++ src/hidden_test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import App from './App';
-import { UrlViewer } from 'react-vzome';
+import { UrlViewer } from '@vzome/react-vzome';
 
 class HiddenTest extends App {
   constructor(props) {
